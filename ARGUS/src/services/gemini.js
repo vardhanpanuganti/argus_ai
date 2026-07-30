465acbbd8ba6eb698732eb1a6626d6f2c204e48a
vardhanpanuganti <vardhanpanuganti001@gmail.com>
2026-07-30 23:48:33 +0530

    argus_ai

--- ARGUS/src/services/gemini.js
+++ ARGUS/src/services/gemini.js
@@ -1,6 +1,6 @@
 export async function generateTravelPlan(prompt) {
 
-  const response = await fetch("https://argus-ai-backend-9wd0.onrender.com", {
+  const response = await fetch("https://argus-ai-backend-9wd0.onrender.com/api/chat", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
